7dd35c7b8e06305b452bc1e0d55a31ce5ddc2163
RayLv <Ray.Lv@jmix.cn>
2022-04-22 11:04:05 +0800

    initialize

---
+++ ImageCropper/imagecropper/src/test/java/cn/jmix/imagecropper/ImgcropTest.java
@@ -0,0 +1,14 @@
+package cn.jmix.imagecropper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ImgcropTest {
+
+	@Test
+	void contextLoads() {
+	}
+}
---
+++ ImageCropper/imagecropper/src/test/java/cn/jmix/imagecropper/ImgcropTestConfiguration.java
@@ -0,0 +1,28 @@
+package cn.jmix.imagecropper;
+
+import io.jmix.core.annotation.JmixModule;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@SpringBootConfiguration
+@EnableAutoConfiguration
+@Import(ImgcropConfiguration.class)
+@JmixModule(id = "cn.jmix.imagecropper.test", dependsOn = ImgcropConfiguration.class)
+public class ImgcropTestConfiguration {
+
+    @Bean
+    @Primary
+    DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .generateUniqueName(true)
+                .setType(EmbeddedDatabaseType.HSQL)
+                .build();
+    }
+}
